872c1c8a051be3c427ebf21bdc9dd6eddd69ed36
sanitto <54099526+sanitto@users.noreply.github.com>
2021-05-16 15:03:53 +0300

    trying again

--- personal_data/personal_data/settings.py
+++ personal_data/personal_data/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '8657815c-11d8-4d2a-9c78-bbdb91695f1a'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application references
 # https://docs.djangoproject.com/en/2.1/ref/settings/#std:setting-INSTALLED_APPS
